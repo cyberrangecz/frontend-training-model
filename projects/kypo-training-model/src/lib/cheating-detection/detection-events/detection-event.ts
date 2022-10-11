460ff1ade6a07bc64356bc61e3c75b40f56a0f9c
xmajdan <445519@mail.muni.cz>
2022-10-11 14:13:39 +0200

    Prettier-fix.

--- projects/kypo-training-model/src/lib/cheating-detection/detection-events/detection-event.ts
+++ projects/kypo-training-model/src/lib/cheating-detection/detection-events/detection-event.ts
@@ -4,7 +4,7 @@
 import { AbstractDetectionEventTypeEnum } from '../../enums/abstract-detection-event-type.enum';
 
 export class AbstractDetectionEvent {
-  id : number;
+  id: number;
   trainingInstanceId: number;
   cheatingDetectionId: number;
   detectionEventType: AbstractDetectionEventTypeEnum;
