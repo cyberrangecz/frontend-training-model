d0b189c7f0f3278476ade62fa37b76c8ee20fab5
Lukáš Majdan <445519@mail.muni.cz>
2024-03-21 20:17:06 +0100

    Develop

--- projects/kypo-training-model/src/lib/cheating-detection/detection-events/detected-forbidden-command.ts
+++ projects/kypo-training-model/src/lib/cheating-detection/detection-events/detected-forbidden-command.ts
@@ -2,11 +2,11 @@
  * This class represents a forbidden command
  * used in a forbidden command detection event
  */
+import { DetectedForbiddenCommandTypeEnum } from '../../enums/detected-forbidden-command-type.enum';
 
 export class DetectedForbiddenCommand {
   command: string;
-  type: string;
-  detectionEventId: number;
+  type: DetectedForbiddenCommandTypeEnum;
   hostname: string;
   occurredAt: Date;
 }
---
+++ projects/kypo-training-model/src/lib/enums/detected-forbidden-command-type.enum.ts
@@ -0,0 +1,4 @@
+export enum DetectedForbiddenCommandTypeEnum {
+  Bash = 'BASH',
+  Msf = 'MSF',
+}
